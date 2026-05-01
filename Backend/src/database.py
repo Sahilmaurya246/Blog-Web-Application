03b7b519f5c08b76d2e6b67ee81ea0c6f79604f5
Sahil Maurya <sahil@example.com>
2026-05-01 21:24:26 +0530

    database change

--- Backend/src/database.py
+++ Backend/src/database.py
@@ -3,11 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URL = "sqlite:///./Blog.db"
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "Blog.db")
-DATABASE_URL = f"sqlite:///{DB_PATH}"
-
+DATABASE_URL = "sqlite:///./Blog.db"
 engine = create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
 SessionLocal=sessionmaker(bind=engine)
 Base=declarative_base()
